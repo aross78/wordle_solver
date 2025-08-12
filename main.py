935283eb6a6bb654a9526910191cdea44c4d55b2
Alyssa Ross <aross@intersystems.com>
2025-08-12 12:50:13 -0400

    Comments

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import itertools
 
-inc = "nam"
+included = "nam"
+excluded = "crehu"
 abc = "abcdefghijklmnopqrstuvwxyz"
-out = "crehu"
-for l in out:
+for l in excluded:
     abc = abc.replace(l, "")
 
 with open('solver\english.txt', 'r') as file:
+    # Convert repo file to iterable
     lines = file.readlines()
-    string_set = {line.strip() for line in lines}
+    string_set = { line.strip() for line in lines }
 
-    options = set()
+    # This part's the definition of brute force :-)
+    possible_solutions = set()
     for first in abc:
         for second in abc:
-            possible = inc + first + second
-            perm_iter = itertools.permutations(possible)
-            base_options = ["".join(p) for p in perm_iter]
+            possible_letters = included + first + second
 
+            # Given a string, returns iterator of all permutations of that string ... 
+            options = [ "".join(p) for p in itertools.permutations(possible_letters) ]
 
-            for o in base_options:
+            # Keep permutations that are valid english words
+            for o in options:
                 if o in string_set:
-                    options.add(o)
+                    possible_solutions.add(o)
 
-    to_remove = {o for o in options 
+    # Additional filtering
+    possible_solutions -= {o for o in possible_solutions 
                  if o[4] in {'m', 'n'} or o[3] != 'a'or o[2] != 'm'}
 
-    options -= to_remove
-
-    print(options)
+    print(possible_solutions)
