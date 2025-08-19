f86dec2229dee09e7dd36544d7329c58166e538e
Alyssa Ross <aross@intersystems.com>
2025-08-19 14:26:55 -0400

    Restructured Solver, merged update_viable_letters w submit_guess for now

--- Solver.py
+++ Solver.py
@@ -1,101 +1,105 @@
 from itertools import product
 import Game
 
+ABC = "abcdefghijklmnopqrstuvwxyz"
+
 class Solver:
-    def __init__(self, game=None):
-        self.abc = set("abcdefghijklmnopqrstuvwxyz")
-        self.viable_letters = [ set(self.abc) for _ in range(5) ]
-
+    def __init__(self, game:Game=None):
+        self.dict = self.init_dict()
+        self.viable_letters = [ set(ABC) for _ in range(5) ]
         self.sln = [""] * 5
-        self.yellows = {} # dict of format {letter: (invalid positions)}          
-        self.blacks = set()
-
+        self.yellows = {} # dict of format {letter: (invalid positions)}, can maybe be improved to encode multiples         
+        self.blacks = set() # this prob needs to be changed to a structure that can encode guessing multiples
         self.game = game
 
-        # For testing
-        #self.sln = ["", "", "m", "a", ""]
-        #self.yellows = {'m': {4}, 'n':{4}}
-        #self.blacks = set("cre")
-        
-        self.dict = self.init_dict()
+    def is_unsolved(self):
+        """
+        Checks if any unknown letters remain in sln
+        """
+        return self.sln.count("")
 
-    def update_viable_letters(self):
-        # Handle greens
-        for i in range(5):
-            # If we have a letter at that pos in sln, it's the only possible letter at that index
-            if self.sln[i]:
-                self.viable_letters[i] = set(self.sln[i])
-
-        # Handle yellows
-        for letter, invalids in self.yellows.items():
-            for i in invalids:
-                self.viable_letters[i].discard(letter)
-        
-        # Handle grays
-        for i in range(5):
-            self.viable_letters[i] -= self.blacks
-    
-        return
-
-    # Returns list of guesses sorted by value in self.dict
     def get_guesses(self):
+        """
+        Based on remaining viable letters, 
+        Returns list of format [(guess, frequency)] sorted by freq defined in self.dict
+        """
         guesses = {}
         for p in product(*self.viable_letters):
            w = "".join(p)
            if w in self.dict.keys():
-               # check if all yellows and greens are a subset of the word
-               # prob going to check this in a way at first where I haven't thought abt edge cases
+                # Lazily checks that all yellows (and greens, implicitly) are in the word and blacks NOT in the word
+                # Have not thought through edge cases nor considered more efficient approaches
+                # The blacks check should be able to be done implicitly too I think
                 if set(self.yellows.keys()).issubset(set(w)):
-                    # 'copy' to our guesses dict
                     guesses[w] = self.dict[w]
-        
+
         return sorted(guesses.items(), key=lambda item: item[1])
     
-    # Interfaces with Game to submit a guess and then update solver's internal model of viable letters
     def submit_guess(self, guess):
+        """
+        Interfaces with Game to submit a guess
+        And then update Solver's internal viable_letters model
+        """
         if self.game:
             result = self.game.score_guess(guess)
         else:
             result = input(f"Input result in format \"gybbg\" where g: green, y: yellow, b: black for guess \"{guess}\": ")
+            while len(result) != 5:
+                result = input(f"Input 5-char result in format \"gybbg\" where g: green, y: yellow, b: black for guess \"{guess}\": ")
 
-        # Update "greens", yellows, and blacks tracking according to response received
+        # Update "greens", yellows, and blacks tracking according to response
+        # Does not currently handle 2 yellows of the same letter properly
+        # Or one green, one black of same letter in same guess
+        # Or a lot of other edge cases...
         for i in range(5):
             c = guess[i]
             score = result[i]
 
             if score == "g":
                 self.sln[i] = c
+                self.viable_letters[i] = set(self.sln[i])
             
             elif score == "y":
                 self.yellows.setdefault(c, set())
                 self.yellows[c].add(i)
+                self.viable_letters[i].discard(c)
 
             else:
-                self.blacks.add(c)
+                # I did this part v lazily
+                # DO NOT TRUST AGAINST EDGE CASES
+                appears_elsewhere = False
+                if c in self.yellows.keys():
+                    appears_elsewhere = True
+                    self.yellows[c].add(i)
+                    for pos in self.yellows[c]:
+                        self.viable_letters[pos].discard(c)
+                    
+                if c in self.sln:
+                    appears_elsewhere = True
+                    for pos in range(5):
+                        if self.sln[pos] != c:
+                            self.viable_letters[pos].discard(c)
+                    
+                if not appears_elsewhere:
+                    self.blacks.add(c)
+                    for pos in self.viable_letters:
+                        pos.discard(c)
         
-        # Update model
-        self.update_viable_letters()
+        # For testing but also kinda useful for visualizing results from CLI
         print(f"Blacks: {self.blacks}, yellows: {self.yellows}, greens: {self.sln}")
-        print(f"By posn: { [ print(r) for r in self.viable_letters ]}")
+        _ = [ print(r) for r in self.viable_letters ]
         
-        # Report sln found if that's the case else recommend guesses
-        if not self.sln.count(""):
-            w = "".join(self.sln)
-            print(f"The solution is {w}")
+        if not self.is_unsolved():
+            print(f"The solution is {"".join(self.sln).upper()}")
         else:
-            next_guesses = self.get_guesses()
-            print(f"Possible next guesses in order: {[w[0] for w in next_guesses]}")
+            print(f"Possible next guesses in order: {[w[0] for w in self.get_guesses()]}")
         return
     
-    def is_unsolved(self):
-        return self.sln.count("")
-
+    
     def init_dict(self, file_path="curated_sorted.txt"):
+        """
+        Given a file path for words sorted by descending commonness, 
+        Populate dict of format {word: frequency_rank}
+        """
         with open(file_path, 'r') as file:
             return { line[1].strip() : line[0] for line in enumerate(file) }
-            #return [ line.strip() for line in file.readlines() ] #orig
-        
-# Testing
-# a = Solver()
-# a.update_viable_letters()
-# a.get_guesses()
